e9e57716e69f207d66b0469b2afd64bc2a95dd88
Bear_ka <gulidova.elena@gmail.com>
2018-08-10 13:30:50 +0300

    lesson 4 2018-08-07

--- src/test/java/LinkedinLoginTest.java
+++ src/test/java/LinkedinLoginTest.java
@@ -3,49 +3,52 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import static java.lang.Thread.sleep;
 
 public class LinkedinLoginTest {
 
-    @Test
-    public void successfulLoginTest(){
-        WebDriver browser = new FirefoxDriver();
+    WebDriver browser;
+
+    @BeforeMethod
+    public void beforeMethod(){
+        browser = new FirefoxDriver();
         browser.get("https://www.linkedin.com/");
-        WebElement userEmailField = browser.findElement(By.xpath("//input[@id='login-email']'"));
-        WebElement userPasswordField = browser.findElement(By.xpath("//input[@id='login-password']"));
-        WebElement signInButton = browser.findElement(By.xpath("//input[@id='login-submit']"));
+    }
 
-        userEmailField.sendKeys("gulidova.elena@gmail.com");
-        userPasswordField.sendKeys("");
-        signInButton.click();
+    @AfterMethod
+    public void afterMethod(){
+        browser.close();
+    }
 
-        WebElement profileDropdown = browser.findElement(By.xpath("//*[@id='nav-settings__dropdown-trigger']"));
+    @Test
+    public void successfulLoginTest() throws InterruptedException {
+        LinkedinLoginPage linkedinLoginPage = new LinkedinLoginPage(browser);
+        linkedinLoginPage.logIn("hellienathornton@gmail.com", "massaraksh");
+        sleep(3000);
+
         String pageUrl = browser.getCurrentUrl();
         String pageTitle = browser.getTitle();
+        WebElement profileDropdown = browser.findElement(By.xpath("//*[@id='profile-nav-item']"));
 
-        browser.close();
-        Assert.assertTrue(profileDropdown.isDisplayed(), "No profile dropdown on logged in page");
-        Assert.assertTrue(pageUrl.contains("/feed"), "Incorrect page url");
-        Assert.assertTrue(pageTitle.contains("LinkedIn"), "Incorrect page title");
+        Assert.assertTrue(profileDropdown.isDisplayed(), "No profile dropdown on home page");
+        Assert.assertEquals(pageUrl, "https://www.linkedin.com/feed/", "Home page url incorrect");
+        Assert.assertEquals(pageTitle, "LinkedIn", "Home page title incorrect");
 
     }
 
     @Test
-    public void negativeLoginTest(){
-        WebDriver browser = new FirefoxDriver();
-        browser.get("https://www.linkedin.com/");
-        WebElement userEmailField = browser.findElement(By.xpath("//input[@id=\"login-email\"]"));
-        WebElement userPasswordField = browser.findElement(By.xpath("//input[@id=\"login-password\"]"));
-        WebElement signInButton = browser.findElement(By.xpath("//input[@id=\"login-submit\"]"));
-
-        userEmailField.sendKeys("a@b.c");
-        userPasswordField.sendKeys("123");
-        signInButton.click();
+    public void negativeLoginTest() throws InterruptedException {
+        LinkedinLoginPage linkedinLoginPage = new LinkedinLoginPage(browser);
+        linkedinLoginPage.logIn("a@b.c", "123");
+        sleep(3000);
 
         WebElement allertBox = browser.findElement(By.xpath("//*[@role='alert']"));
 
         Assert.assertEquals(allertBox.getText(), "There were one or more errors in your submission. Please correct the marked fields below.", "Incorrect message in alert box");
-        browser.close();
     }
 
 
